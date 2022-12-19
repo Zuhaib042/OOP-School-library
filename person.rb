01719c4d0e8aad7246dd8969fec97442d846e53f
Zuhaib Amjad <umar0423@gmail.com>
2022-12-19 23:03:06 +0500

    Add teacher.rb file and fix linters errors

--- person.rb
+++ person.rb
@@ -1,26 +1,21 @@
 class Person
-    attr_accessor :name, :age
-    attr_reader :id
-    def initialize(name = 'Unknown', age, parent_permission = true)
-        @id = Random.rand(1..1000)
-        @name = name
-        @age = age
-    end
+  attr_accessor :name, :age
+  attr_reader :id
 
-    def can_use_services?
-        if is_of_age? || parent_permission
-            true
-        end
-    end
-    
-    private
-    def is_of_age?
-        if @age >= 18
-            true
-        else
-            false
-        end
-    end
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    of_age? || parent_permission ? true : false
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
 end
-    
-    
--- student.rb
+++ student.rb
@@ -1,11 +1,12 @@
+require_relative 'person'
+
 class Student < Person
-    def initialize(classroom)
-        super()
-        @classroom = classroom
-    end
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(name, age, parent_permission)
+    @classroom = classroom
+  end
 
-    def play_hooky
-        puts "¯\(ツ)/¯"
-    end
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
 end
-
