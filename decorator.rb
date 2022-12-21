dc3d8b857cafdde3e756cae3bc681db85cf3a656
Zuhaib Amjad <umar0423@gmail.com>
2022-12-21 15:31:44 +0500

    fix linters and other small issues

--- decorator.rb
+++ decorator.rb
@@ -1,10 +1,12 @@
-# require './nameable.rb'
+require_relative 'nameable'
 
-# class Decorator < Nameable
-#   def initialize(nameable)
-#     @nameable = Nameable.new
-#   end
-#   def correct_name
+class Decorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
 
-#   end
-# end
+  def correct_name
+    @nameable.correct_name
+  end
+end
--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,8 +2,6 @@ require_relative 'decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    if @nameable.correct_name.length > 10
-      @nameable.correct_name[0,10]
-    end
+    @nameable.correct_name[0, 10] if @nameable.correct_name.length > 10
   end
 end
