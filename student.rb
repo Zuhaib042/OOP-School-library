752f9eefab0f9089959b68c9d26faf54f1ba8599
Zuhaib Amjad <umar0423@gmail.com>
2022-12-23 04:28:57 +0500

    Update student class according to the project requirement

--- student.rb
+++ student.rb
@@ -2,10 +2,11 @@ require_relative 'person'
 
 class Student < Person
   attr_reader :classroom
-
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(name, age, parent_permission)
+  attr_accessor :type
+  def initialize(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+    @type = 'Student'
   end
 
   def classroom=(classroom)
