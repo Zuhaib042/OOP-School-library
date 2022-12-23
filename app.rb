0f1f113db8209af302ac96aa26948ee6dcc3fe15
Zuhaib Amjad <umar0423@gmail.com>
2022-12-23 15:03:39 +0500

    Fix a loop bug and linters errors

--- app.rb
+++ app.rb
@@ -4,16 +4,15 @@ require_relative 'student'
 require_relative 'rental'
 
 class App
-
   def initialize
     @persons = []
     @books = []
     @rentals = []
   end
-  
+
   def run
-  puts 'Welcome to School Library App!'
-  menu
+    puts 'Welcome to School Library App!'
+    menu
   end
 
   def menu
@@ -29,16 +28,16 @@ class App
       puts '7 - Exit'
       puts ''
       input = gets.chomp.to_i
-      options(input)
-      if input == 7
-        puts 'Thank you for using our app.'
+      case input
+      when 1..6 then options(input)
+      when 7 then puts 'Thank you for using our app.'
       end
-      break
+      break if input == 7
     end
   end
 
   def options(input)
-    case (input)
+    case input
     when 1
       list_books
     when 2
@@ -54,14 +53,10 @@ class App
     end
   end
 
-  def menu_loop
-   menu
-  end
-
   def create_person
     puts 'Do you want to create a student (1) or a teacher (2) [input the number]: '
     input = gets.chomp.to_i
-    case (input)
+    case input
     when 1
       create_student
     when 2
@@ -70,7 +65,7 @@ class App
       puts 'Please enter a valid number: 1 or 2'
     end
   end
-  
+
   def create_student
     print 'Age: '
     age = gets.chomp.to_i
@@ -81,7 +76,6 @@ class App
     each_student = Student.new(age, name, permission)
     @persons.push(each_student)
     puts "Student '#{name}' created successfully"
-    menu_loop
   end
 
   def create_teacher
@@ -94,7 +88,6 @@ class App
     each_teacher = Teacher.new(age, specialization, name)
     @persons.push(each_teacher)
     puts "Teacher '#{name}' created successfully"
-    menu_loop
   end
 
   def create_book
@@ -105,72 +98,58 @@ class App
     each_book = Book.new(title, author)
     @books.push(each_book)
     puts "Book '#{title}' created successfully"
-    menu_loop
   end
 
   def list_books
-   if @books.empty?
-    puts 'Sorry, there is no book in the Collection'
-    menu_loop
-   else
-    @books.each_with_index do |book, index| 
-      puts "#{index}- Title: '#{book.title}', Author: #{book.author}"
+    if @books.empty?
+      puts 'Sorry, there is no book in the Collection'
+    else
+      @books.each_with_index do |book, index|
+        puts "#{index}- Title: '#{book.title}', Author: #{book.author}"
+      end
     end
-    menu_loop
-   end
   end
 
   def list_people
     if @persons.empty?
       puts 'Sorry, there is no person in the Database'
-      menu_loop
     else
       @persons.each_with_index do |person, index|
         puts "#{index}- [#{person.type}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
       end
-      menu_loop
     end
   end
 
   def create_rental
     if @books.empty?
       puts 'Sorry, there is no book in the collection to rent out.'
-      menu_loop
     elsif @persons.empty?
       puts 'Sorry, there is no person in the Database. Add a person first in the database'
-      menu_loop
     else
-     puts 'Select a book from the following list by number: '
-     @books.each_with_index do |book, index| 
-      puts "#{index}- Title: '#{book.title}', Author: #{book.author}"
-     end
-     book_index = gets.chomp.to_i
-     book = @books[book_index]
-     puts 'Select a person from the following list by number (not id): '
+      puts 'Select a book from the following list by number: '
+      @books.each_with_index { |book, i| puts "#{i}- Title:'#{book.title}', Author: #{book.author}" }
+      book_index = gets.chomp.to_i
+      book = @books[book_index]
+      puts 'Select a person from the following list by number (not id): '
       @persons.each_with_index do |person, index|
-       puts "#{index}- [#{person.type}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+        puts "#{index}- [#{person.type}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
       end
-     person_id = gets.chomp.to_i
-     person = @persons[person_id]
-     print 'Date: '
-     date = gets.chomp
-     rental = Rental.new(date, person, book)
-     @rentals.push(rental)
-     puts "Rental created successfully"
-     menu_loop
+      person_id = gets.chomp.to_i
+      person = @persons[person_id]
+      print 'Date: '
+      date = gets.chomp
+      rental = Rental.new(date, person, book)
+      @rentals.push(rental)
+      puts 'Rental created successfully'
     end
   end
-  
+
   def list_all_rentals
     puts 'ID of person: '
     person_id = gets.chomp.to_i
     puts 'Rentals: '
     @rentals.each do |rental|
-      if person_id == rental.person.id
-        puts "Date: #{rental.date}, Book '#{rental.book.title}' by #{rental.book.author}"
-      end
+      puts "Date: #{rental.date}, Book '#{rental.book.title}' by #{rental.book.author}" if person_id == rental.person.id
     end
-    menu_loop
   end
-
 end
