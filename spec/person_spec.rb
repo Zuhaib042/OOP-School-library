3fc7cbcc09fd3f554329b15d4c3d30f63326fbed
Zuhaib Amjad <umar0423@gmail.com>
2023-01-05 13:17:03 +0500

    Add testing of method `can_use_services?` to the class of Person
    in spec files

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -5,13 +5,15 @@ require_relative '../person'
 describe Person do
   context 'Testing Person class' do
     it 'Should be instance of Person' do
-      person = Person.new('Lily', 24)
+      person = Person.new(24, 'Lily')
       expect(person).to be_instance_of Person
 
       book = Book.new('Sapiens', 'Joshua')
 
       person.add_rental('12-12-2020', book)
       expect(person.rentals[0]).to be_instance_of Rental
+
+      expect(person.can_use_services?).to be_truthy
     end
   end
 end
